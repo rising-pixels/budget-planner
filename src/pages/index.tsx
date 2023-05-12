5d1a63e58b3aeeb6c84e8e746e842456fbef32ee
Konrad <konslo96@gmail.com>
2023-05-12 20:09:36 +0200

    feat: Implement Dexie.js into the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Dexie, { Table } from "dexie";
 
-const HomePage = () => {
-  return (
-    <p>Hello World</p>
-  )
+export interface LuckyNumber {
+  number: number;
 }
 
-export default HomePage
+export class MySubclassedDexie extends Dexie {
+  luckyNumber: Table<LuckyNumber>;
+
+  constructor() {
+    super("budgetPlannerDatabase");
+    this.version(1).stores({
+      luckyNumber: "number",
+    });
+  }
+}
+
+const db = new MySubclassedDexie();
+
+const HomePage = () => {
+  const [luckyNumber, setLuckyNumber] = useState<number | null>(null);
+
+  const drawNumberHandler = async () => {
+    try {
+      db.luckyNumber.clear();
+      const randomNumber = Math.floor(Math.random() * 11);
+      setLuckyNumber(randomNumber);
+      await db.luckyNumber.add({ number: randomNumber });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const clearDataHandler = async () => {
+    try {
+      await db.luckyNumber.clear();
+      setLuckyNumber(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const getLuckyNumber = async () => {
+      try {
+        const data = await db.luckyNumber.toArray();
+        if (data.length > 0) {
+          setLuckyNumber(data[0].number);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getLuckyNumber();
+  }, []);
+
+  return (
+    <>
+      <p>Hello World</p>
+      <button onClick={clearDataHandler}>Clear Data</button>
+      <button onClick={drawNumberHandler}>Draw a number</button>
+      {luckyNumber !== null ? (
+        <p>Today lucky number is {luckyNumber}</p>
+      ) : (
+        <p>No luck today</p>
+      )}
+    </>
+  );
+};
+
+export default HomePage;
