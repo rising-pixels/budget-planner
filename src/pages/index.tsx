4f1ed1c17f4f9d79ba8d2cafc9142d3cbc9b421c
Lalasimon <lalaszymon@gmail.com>
2023-05-15 13:45:58 +0200

    empty {} added to prettierrc file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <p>Hello World</p>
-  )
-}
+  return <p>Hello World</p>;
+};
 
-export default HomePage
+export default HomePage;
