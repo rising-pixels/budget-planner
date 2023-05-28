b4f2a3f28e29f85f4e8ef3c4e04476524d23b58e
Jakub Kołuda <jko01@outlook.com>
2023-05-28 10:58:33 +0200

    change deleteCategory

--- src/categories/services/deleteCategory.ts
+++ src/categories/services/deleteCategory.ts
@@ -1,11 +1,5 @@
 import { MySubclassedDexie } from "../../pages";
-import { categoryUuidExists } from "../validators/categoryExists";
 
 export const deleteCategory = async (db: MySubclassedDexie, uuid: string) => {
-  const existingCategories = await db.categories.toArray();
-  if (categoryUuidExists(existingCategories, uuid)) {
-    return db.categories.delete(uuid);
-  } else {
-    throw new Error("This category does not exist");
-  }
+  return db.categories.delete(uuid);
 };
